32af5f7226ae6ce254290c7bc12a0605c2a29020
rPrinceA495 <princepappiah@gmail.com>
2020-10-20 11:47:47 +0200

    Login functionality

--- app/src/main/java/com/example/pedalpals/Login.java
+++ app/src/main/java/com/example/pedalpals/Login.java
@@ -1,14 +1,71 @@
 package com.example.pedalpals;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+
+    private EditText loginemail, loginpass;
+    private ImageButton loginbtn;
+    private ProgressBar loginprogressBar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        loginemail = findViewById(R.id.loginemail);
+        loginpass = findViewById(R.id.loginpass);
+        loginbtn = findViewById(R.id.loginbtn);
+        loginprogressBar = findViewById(R.id.loginprogressBar);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginemail.getText().toString().trim();
+                String password = loginpass.getText().toString().trim();
+
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+
+                            FirebaseUser user = auth.getCurrentUser();
+                            startActivity(new Intent(Login.this, Dashboard.class));
+                            finish();
+
+                        }else {
+
+                            Toast.makeText(Login.this, "Sign in failed." + task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+
     }
 }
